b735d3dc852484c29b597ec89c1be11e3651342c
sunjae0902 <sunjaegg0902@naver.com>
2025-08-10 14:40:46 +0000

    fix: 뒤로가기 아이콘 안보이는 이슈 해결
    - lucid icon 문제라 커스텀 에셋으로 변경

--- src/features/common/IconButton.tsx
+++ src/features/common/IconButton.tsx
@@ -18,7 +18,7 @@ export default function IconButton({
   isActive,
   onClick,
   alt = iconName,
-  className = "aspect-square w-46",
+  className = "aspect-square w-46 cursor-pointer",
   style = {},
 }: IconButtonProps) {
   const imgSrc =
