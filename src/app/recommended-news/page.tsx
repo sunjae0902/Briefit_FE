e78e505313cbd0c62acfeb44308176704eb02c8e
sunjae0902 <sunjaegg0902@naver.com>
2025-08-19 03:33:49 +0000

    fix

--- src/app/recommended-news/page.tsx
+++ src/app/recommended-news/page.tsx
@@ -1,13 +1,13 @@
-import RefreshOnBackWrapper from "@/components/RefreshOnBackWrapper";
+// import RefreshOnBackWrapper from "@/components/RefreshOnBackWrapper";
 import RecommendedNews from "@/features/recommended-news/components/RecommendedNews";
 
 export default function RecommendedNewsPage() {
   return (
     <div className="space-y-15">
       <div className="font-title-24">나의 추천 뉴스</div>
-      <RefreshOnBackWrapper>
+      {/* <RefreshOnBackWrapper> */}
         <RecommendedNews />
-      </RefreshOnBackWrapper>
+      {/* </RefreshOnBackWrapper> */}
     </div>
   );
 }
